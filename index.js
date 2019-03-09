fbf388d0cac5fa5b85cca4d90bc7227d49bae2f9
americanman <snowboardracer7@gmail.com>
2019-03-09 12:41:34 -0500

    Add second method for truncating

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const truncateString = (str, num) => {
   // Clear out that junk in your trunk
+  
   if(num < str.length) {
-  let newStr = '';
-    for(let i = 0; i < num; i++) {
-      newStr += str[i]
-    }
-    newStr += '...'
-    return newStr;
+    let newArr = str.substring(0, num) + '...'
+    return newArr
   }
   else {
       return str
   }
+
+//  if(num < str.length) {
+//  let newStr = '';
+//    for(let i = 0; i < num; i++) {
+//      newStr += str[i]
+//    }
+//    newStr += '...'
+//    return newStr;
+//  }
+//  else {
+//      return str
+//  }
 }
 
 const truncated = truncateString("A-tisket a-tasket A green and yellow basket", 8); 
